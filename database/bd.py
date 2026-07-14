558bd7b68be57a75f2e8f9c638640dc4ba567336
Magenta <glebpolivoda@gmail.com>
2026-07-14 23:36:02 +0300

    Secret

--- database/bd.py
+++ database/bd.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://practic_user:practic_pass@localhost/practic_db"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_PORT}/{DB_NAME}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
